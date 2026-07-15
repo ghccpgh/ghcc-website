14c1964b0695361458cfdadb3214fecb9cd5885a
ManitBhargave <manit.bhargave@gmail.com>
2026-07-14 22:57:59 -0400

    Remove mission section from website and Sanity schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,10 +2,9 @@ import { type SchemaTypeDefinition } from 'sanity'
 import { post } from './documents/post'
 import { announcement } from './documents/announcement'
 import { testimonial } from './documents/testimonial'
-import { mission } from './documents/mission'
 import { boardMember } from './documents/boardMember'
 import { partner } from './documents/partner'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, announcement, testimonial, mission, boardMember, partner],
+  types: [post, announcement, testimonial, boardMember, partner],
 }
